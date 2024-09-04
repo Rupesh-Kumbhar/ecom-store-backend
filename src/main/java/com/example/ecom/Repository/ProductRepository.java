7f09e0b0efa320eede25fdba26de50394393a064
RUPESH KUMBHAR <rupesh.p.kumbhar@gmail.com>
2024-09-04 23:51:45 +0530

    Able to create Product Table... Controller, Model, Repository, Service

---
+++ src/main/java/com/example/ecom/Repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.example.ecom.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.ecom.Model.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Integer> {
+	
+}
