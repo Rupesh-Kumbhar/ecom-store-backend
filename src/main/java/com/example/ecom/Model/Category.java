f65db1e3e7ca7eae5cf565b8291cee82b5b56de1
RUPESH KUMBHAR <rupesh.p.kumbhar@gmail.com>
2024-11-29 13:16:25 +0530

    Category Controller, Service, Repository, Dto All Added

---
+++ src/main/java/com/example/ecom/Model/Category.java
@@ -0,0 +1,44 @@
+package com.example.ecom.Model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Category {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int categoryId;
+	private String categoryName;
+	
+
+	public Category() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Category(int categoryId, String categoryName) {
+		super();
+		this.categoryId = categoryId;
+		this.categoryName = categoryName;
+	}
+
+	public int getCategoryId() {
+		return categoryId;
+	}
+
+	public void setCategoryId(int categoryId) {
+		this.categoryId = categoryId;
+	}
+
+	public String getCategoryName() {
+		return categoryName;
+	}
+
+	public void setCategoryName(String categoryName) {
+		this.categoryName = categoryName;
+	}
+	
+}
---
+++ src/main/java/com/example/ecom/Service/CategoryService.java
@@ -0,0 +1,59 @@
+package com.example.ecom.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.ecom.Dto.CategoryDto;
+import com.example.ecom.Exception.ResourceNotFoundException;
+import com.example.ecom.Repository.CategoryRepository;
+import com.example.ecom.Model.Category;
+
+@Service
+public class CategoryService {
+	
+	@Autowired
+	private CategoryRepository catRepo;
+	@Autowired
+	private ModelMapper mapper;
+	
+	public CategoryDto createCategory(CategoryDto catDto) {
+		//CatDto to CatEntity
+		Category cat = this.mapper.map(catDto, Category.class); 
+		Category save = this.catRepo.save(cat);
+		
+		// Save returning Entity
+		
+		//CatEntity to Dto
+		CategoryDto saveDto = this.mapper.map(save,CategoryDto.class);
+		return saveDto; 
+	}
+	
+	public CategoryDto updateCategory(CategoryDto newCat, int catId) {
+		Category oldCat = this.catRepo.findById(catId).orElseThrow(()-> new ResourceNotFoundException("This Category Id not found") );
+//		oldCat.setCategoryId(newCat.getCategoryId());
+		oldCat.setCategoryName(newCat.getCategoryName());
+		Category save = this.catRepo.save(oldCat);
+		return this.mapper.map(save, CategoryDto.class);
+	} 
+	
+	public void deleteCategoryById(int catId){
+		Category cat = this.catRepo.findById(catId).orElseThrow(()-> new ResourceNotFoundException("Category not found for deletion"));
+		this.catRepo.delete(cat);
+	}
+	
+	public CategoryDto viewCategoryById(int catId){
+		Category viewCatId = this.catRepo.findById(catId).orElseThrow(()-> new ResourceNotFoundException("Category not found for Viewing"));
+		return this.mapper.map(viewCatId, CategoryDto.class);
+	}
+	
+	public List<CategoryDto> viewCategoryAll(){
+		List<Category> catAll = this.catRepo.findAll();
+		List<CategoryDto> catAllDto =  catAll.stream().map(cat ->this.mapper.map(cat, CategoryDto.class)).collect(Collectors.toList());
+		return catAllDto;
+	}
+}
